557e0d3144226d5e349e22207ee1c12aa40e4d93
elizabethfong <elizabethfongwm@gmail.com>
2015-05-03 14:32:33 -0400

    commented gpsReader.py

--- ObjectTransformation/gpsReader.py
+++ ObjectTransformation/gpsReader.py
@@ -1,42 +1,54 @@
 from pynmea.pynmea import nmea
-#*import matplotlib.pyplot as plt
+
 import pyserial.serial as serial, time, sys, threading, datetime, shutil
 import pyserial.serial.serialutil as serialutil
 import pyserial.serial.serialwin32 as serialwin32
 
 
 class GPSReader():
+	"""
+	GPS Reader for reading GPS data.
 
-	ser = None
-	
-	#lat = None
-	#long = None
-	#pos_x = None
-	#pos_y = None 
+	@author: Kim Faughnan, Elizabeth Fong, Spring 2015
+	"""
 
-
+	# gpgga - for geographic lat, long
 	gpggaBAUDRATE = None  
 	gpggaComnum = None
 	gpggaSerial = None
 
+	# gpvtg - for ground speed and bearing
 	gpvtgBAUDRATE = None  
 	gpvtgComnum = None
 	gpvtgSerial = None
 
+	# current position (longitude,latitude)
 	currentPosition = None
 
 
+	# --- Construction -------------------------------------------- #
+
 	def __init__(self, gpggaComnum="COM1", gpggaBaudrate=4800, gpvtgComnum="COM2", gpvtgBaudrate=4800): 
+		"""
+		Constructor: Initialises the GPS reader.
+
+		@return: None.
+		"""
 		self.gpggaBAUDRATE = gpggaBaudrate
 		self.gpggaComnum = gpggaComnum
 
 		self.gpvtgBAUDRATE = gpvtgBaudrate 
 		self.gpvtgComnum = gpvtgComnum 
 
-	def initConnections(self): 
 
-		#ser = serial.Serial()
-		self.gpggaSerial = serialwin32.Serial()#serialwin32.Serial('/dev/tty.GarminGLO47d48-COM7')
+	def initConnections(self): 
+		"""
+		Initialises connections to the bluetooth GPS.
+
+		@return: None.
+		"""
+		# init gpggaSerial
+		self.gpggaSerial = serialwin32.Serial()
 		self.gpggaSerial.baudrate = self.gpggaBAUDRATE
 		self.gpggaSerial.port = self.gpggaComnum
 		self.gpggaSerial.timeout = 1
@@ -46,8 +58,8 @@ class GPSReader():
 		print 'OPEN: '+ self.gpggaSerial.name
 		print '' 
 
-
-		self.gpvtgSerial = serialwin32.Serial()#serialwin32.Serial('/dev/tty.GarminGLO47d48-COM7')
+		# init gpvtgSerial
+		self.gpvtgSerial = serialwin32.Serial()
 		self.gpvtgSerial.baudrate = self.gpvtgBAUDRATE
 		self.gpvtgSerial.port = self.gpvtgComnum
 		self.gpvtgSerial.timeout = 1
@@ -58,23 +70,16 @@ class GPSReader():
 		print '' 
 
 
+	# --- Read GPS Data ------------------------------------------- #
+
 	def getLongLat(self):
-		"""returns (longitude, latitude)"""
-		try:    
-			
-			#line = self.gpggaSerial.readline()    
-			#print ("\n      in newgpsdata calling readlastline") 
-			#line = raw_input()
-			#line = self.gpggaSerial.readlastline()   
-			#print("\n      after line read.")    
-			#line = raw_input()
-
-			# print ("\n      in newgpsdata calling readline") 
-			# raw_input()
-			# line = self.gpggaSerial.readline()   
-			# print("\n      after line read.")    
-			# raw_input()
-
+		"""
+		Returns the geographic coordinates as a duple, in the format (longitude,latitude).
+		
+		@return The geographic coordinates, (longitude,latitude).
+		"""
+		try:
+			# read line
 			lastLine = "0" 
 			line = "0" 
 			while line is not "" or (lastLine is "0" or lastLine is ""):
@@ -82,12 +87,11 @@ class GPSReader():
 				lastLine = line
 				line = self.gpggaSerial.readline()   
 			print("\n      after line read.")    
-			#raw_input()
 
 			line = lastLine
-
 			print "   from gps: ", line  
 			
+			# parse line in NMEA GPGGA format
 			gpgga = nmea.GPGGA() 
 			gpgga.parse(line)
 
@@ -98,9 +102,6 @@ class GPSReader():
 
 			lats = gpgga.latitude
 			longs = gpgga.longitude 
-
-
-			#print("  latitude: {0}\n  longitude: {1}".format(lats,longs))
 			
 			#convert degrees,decimal minutes to decimal degrees 
 			lat1 = (float(lats[2]+lats[3]+lats[4]+lats[5]+lats[6]+lats[7]+lats[8]))/60
@@ -108,25 +109,31 @@ class GPSReader():
 			long1 = (float(longs[3]+longs[4]+longs[5]+longs[6]+longs[7]+longs[8]+longs[9]))/60
 			long = (float(longs[0]+longs[1]+longs[2])+long1)
 			
-			#calc position 
+			#calculate position 
 			pos_y = lat
-			pos_x = -long #longitude is negative  
+			pos_x = -long 	#longitude is negative  
 
 			#shows that we are reading through this loop
 			print ("x: %f"  % pos_x)
 			print ("y: %f"  % pos_y) 
 
 			self.currentPosition = (pos_x,pos_y)
-
 			return (pos_x,pos_y)
 
 		except:
 			print "error in stream_serial ", sys.exc_info()[0]
 			pass 
 
-	def getGroundSpeedAndBearing(self):
-		try:    
 
+	def getGroundSpeedAndBearing(self):
+		"""
+		Returns the ground speed and bearing read from the GPS, as a duple, with the format
+		(ground speed, bearing).
+
+		@return: The ground speed and bearing.
+		"""
+		try:
+			# read line
 			lastLine = "0" 
 			line = "0" 
 			while line is not "" or (lastLine is "0" or lastLine is ""):
@@ -134,12 +141,11 @@ class GPSReader():
 				lastLine = line
 				line = self.gpvtgSerial.readline()   
 			print("\n      after line read.")    
-			#raw_input()
-
+			
 			line = lastLine
-
 			print "   from gps: ", line  
 			
+			# parse line in NMEA GPVTG format
 			gpvtg = nmea.GPVTG() 
 			gpvtg.parse(line)
 
@@ -151,19 +157,9 @@ class GPSReader():
 			bearing = gpvtg.true_track #or mag_track
 			groundspeed = gpvtg.spd_over_grnd_kmph #or for knots end with kts instead of kmph  
 			print ("returning {0} from ground speed bearing".format((groundspeed,bearing)))
+			
 			return (groundspeed,bearing)
+
 		except:
 			print "error in stream_serial ", sys.exc_info()[0]
 			pass  
-
-
-
-
-
-
-
-
-
-
-
-
